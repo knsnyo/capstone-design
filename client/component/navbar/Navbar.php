7a3ea0f7ec5b3df77b0273058bf9e514ae37c0d4
knsnyo <isc963@naver.com>
2022-03-14 11:58:54 +0900

    20220314 html 수정

---
+++ client/component/navbar/Navbar.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="navbar.css">
+  </head>
+  <body>
+    <div class="navbar">
+      <div class="navbarLeft">
+        <ul class="navbarList">
+          <li class="navbarListItem">
+            <a class="link">
+              Home
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="navbarCenter">
+        <ul class="navbarList">
+          <li class="navbarListItem">
+            <a class="link">
+              menu 1
+            </a>
+          </li>
+          <li class="navbarListItem">
+            <a class="link">
+              menu 2
+            </a>
+          </li>
+          <li class="navbarListItem">
+            <a class="link">
+              menu 3
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="navbarRight">
+        <ul class="navbarList">
+          <li class="navbarListItem">
+            <Link to="/login" class="link">
+            LOGIN
+            </Link>
+          </li>
+          <li class="navbarListItem">
+            <Link to="/register" class="link">
+            REGISTER
+            </Link>
+          </li>
+        </ul>
+        <ul class="navbarList">
+          <li class="navbarListItem">
+            <a class="link">
+              MyPage
+            </a>
+          </li>
+          <li class="navbarListItem">LOGOUT</li>
+        </ul>
+      </div>
+    </div>
+  </body>
+</html>
